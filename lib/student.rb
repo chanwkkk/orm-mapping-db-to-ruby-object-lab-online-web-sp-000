ef907cd7f4546c906faa65036b0782c049aa6485
Yingqi Chen <chanwk18@hotmail.com>
2019-05-23 05:08:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,13 @@ class Student
   end
 
   def self.all_students_in_grade_X
+    sql=<<-SQL
+      SELECT * FROM students
+      WHERE grade<12
+    SQL
 
-
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end
   end
 end
