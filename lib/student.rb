44ad7a34755b1fee82e031556ca4687b1a525cc2
Yingqi Chen <chanwk18@hotmail.com>
2019-05-23 04:45:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,9 @@ class Student
       SELECT * FROM students
     SQL
 
-    DB[:conn]
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
